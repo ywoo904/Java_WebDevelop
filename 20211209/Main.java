f2de58f35b99bbb75daab263dbaa4e84353a384e
ywoo904 <ywoo904@gmail.com>
2021-12-09 17:57:49 +0900

    Create Main.java

---
+++ 20211209/Main.java
@@ -0,0 +1,392 @@
+// public class Main {
+//   public static void main (String[] args) {
+  
+//     int [] mouse = {1,2,3,4,5,6,7,8,9};
+//     for (int monitor:mouse) {
+//       System.out.println(monitor);
+
+//     }
+
+
+
+//   }
+// }
+
+
+
+//   }
+// }
+
+// public class Main {
+//   public static void main (String[] args) {
+  
+// String [] newbie= {"a","b","c"};
+
+// for(String bcc:newbie) {
+//   System.out.println(bcc);
+
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 
+// public class Main {
+//   public static void main (String[] args) {
+//     int [] pattern = {1,2,3,4,5};
+
+//     for (int portable:pattern) {
+//       System.out.println(portable);
+//     }
+
+//   }
+
+// }
+
+
+
+
+
+
+
+
+// public class Main {
+//   public static void main (String[] args) {
+//     String [] pattern = {"one","two","three"};
+
+//     for (String tjmedia:pattern) { 
+//       System.out.println(tjmedia);
+//     }
+
+
+//   }
+
+// }
+
+
+
+
+// public class Main {
+
+// public static void main (String[] args) {
+
+// String [] number = {"one","two","three"};
+
+// for (String num: number) {
+
+// System.out.println(num);
+
+// }
+// }
+// }
+
+
+
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int[] iarr = {10,20,30,40,50};
+               
+      
+//         for(int value:iarr) {
+//             System.out.println(value);
+//         }
+//     }
+// }
+
+
+// public class Main { 
+//   public static void main (String[] args) {
+//     String []number={"one","two", "three"}; 
+//     for (String num:number) {
+//       System.out.println(num);
+//     }
+
+
+//   }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Main {
+// public static void main(String[] args) {
+//  int a=15;
+//  int b=20; 
+
+//  System.out.println(a+b);
+//  System.out.println(a-b);
+//  System.out.println(a*b);
+//  System.out.println(a/b);
+
+//  System.out.println(b%a);
+// }
+// }
+
+
+// class Main {
+//   public static void main(String[] args) {
+//       int a = 15;
+//       int b = 20;
+
+//   // System.out.println(++a); 
+//   // System.out.println(a);
+
+//   // System.out.println(a++);
+//   // System.out.println(a); 
+
+//   }
+
+
+// // }
+
+// String a = "Hello World"; //Simplified
+// String a = new String ("Hello World"); //Normally 
+
+
+// class Main {
+//   public static void main (String [] args) {
+//     String a="hello"; 
+//     String b="crypto";
+//     String c="hello";
+    
+//     System.out.println(a.equals(c));
+//     System.out.println(a.equals(b));
+//   }
+
+// }
+
+// class Main {
+//   public static void main(String [] args) {
+//     String a="Hello World";
+    
+//     System.out.println(a.indexOf("World"));
+
+//   }
+
+// }
+
+// class Main{
+//   public static void main (String[] args) {
+//       String a="Hello World";
+      
+//   System.out.println(a.replaceAll("Hello", "Fuck the"));
+
+
+//   }
+
+// }
+
+
+// class Main {
+//   public static void main (String[] args) {
+//       String a="Hello World";
+//       System.out.println(a.substring(0,7));
+//   }
+// }
+
+// class Main {
+//   public static void main(String [] args) {
+//     String a="Hello World";
+    
+//     System.out.println(a.toUpperCase());
+    
+//   }
+
+
+// }
+
+
+// class Main {
+//   public static void main(String [] args){
+//   int base=180;
+//   int height= 230;
+
+//   boolean isTall=height>base;
+//   if(isTall) {
+//     System.out.println("He is so tall");
+
+//   }
+
+//   }
+// }
+
+
+
+// class Main {
+//   public static void main(String [] args) {
+//     int standard =150; 
+//     int height = 180;
+  
+
+// boolean Hehecktall=height>standard; {
+// if(Hehecktall) 
+// System.out.println("Hella tall");
+
+// }
+//   }
+   
+//   }
+
+// class Main {
+//   public static void main(String[] args) {
+//     char a = 'a';
+//     char c = 97;
+
+//     System.out.println(a);
+// System.out.println(c);
+//   }
+
+// }
+
+// class Main {
+//   public static void main(String[] args) {
+//     String [] weeks= new String [7];
+
+//     weeks[0] ="Mon";
+//     weeks[1] ="Tues";
+//   weeks[2] ="Wed";
+//     weeks[3] ="Thurs";
+//   weeks[4] ="Fri";
+//     weeks[5] ="Sat";
+//       weeks[6] ="Sun";
+
+//   System.out.println (weeks[4]);
+//   }
+
+// }
+
+// class Main {
+//   public static void main(String[] arsg) {
+//     String [] weeks= new String [7];
+//      weeks[0] ="Mon"; 
+//     weeks[1] ="Mon"; 
+//     weeks[2] ="Tues";
+//         weeks[3] ="Mon"; 
+//     weeks[4] ="Tues";
+//         weeks[5] ="Mon"; 
+//     weeks[6] ="Tues";
+//     for (int i=0; i< weeks.length; i++) {
+// for (int i=0; i<weeks.length;i++)
+      
+//   System.out.println(weeks[i]);
+//     }
+
+
+// //     }
+// //   }
+// import java.util.ArrayList;
+// public class  Main {
+//   public static void main (String[] args){
+//     ArrayList apple=new ArrayList();
+//     apple.add("123");
+//     apple.add("456");
+//     apple.add("789");
+//     apple.add(0,"133");
+//     System.out.println(apple.get(1));
+//   }
+  
+// }
+
+// import java.util.ArrayList;
+// public class Main {
+// public static void main (String[] args){
+//   ArrayList peaches=new ArrayList ();
+//   peaches.add ("123");
+//   peaches. add ("456"); 
+//   peaches.add ("789");
+//   peaches.add(0,"999")  ;
+//   System.out.println(peaches.get(1));
+
+// }
+
+// }
+
+
+
+// Generic 안썼을때;
+// ArrayList yolo = new yolo (); 
+
+// ArrayList <String> yolo = new ArrayList<String>(); 
+
+// import java.util.ArrayList;
+// class main {
+//   public static void main(String [] args) {
+//     ArrayList mouse=new ArrayList();
+//     mouse.add ("123");
+//     mouse.add ("456"); 
+//     mouse.add(0,"123");
+//     System.out.println(mouse.add(1));
+
+//   }
+// }
+
+
+// ArrayList mouse = new ArrayList (); 
+
+// ArrayList <String> mouse= new ArrayList <String>();
+
+
+// import java.util.ArrayList;
+
+// public class Main {
+//   public static void main (String[] args) {
+//     ArrayList superstar= new ArrayList (); 
+//     superstar.add ("Hello");
+//     superstar.add ("World");
+
+//     String Hello = (String) superstar.get(0);
+//     String World = (String) superstar.get(1);
+
+//   System.out.print("Hello");
+  
+//   }
+
+// }
+// // Generic 사용 시 
+
+
+// import java.util.ArrayList;
+
+// class Main {
+//   public static void main (String[] args){
+//     ArrayList <String>superstar = new ArrayList <String>();
+    
+//     superstar.add ("Hello");
+//     superstar.add ("World"); 
+
+//     String Hello = superstar.get(0);
+//     String World = superstar.get(1); 
+
+//   System.out.println("Hello");
+    
+
+//   }
+
+// }
